90ed8682e88d37ebca9be8825ed2561f1068b51f
paliocodroipo <francesco.tubaro.whatsapp>
2025-07-15 20:26:08 +0200

    stats_g2

--- game_results/game_results.js
+++ game_results/game_results.js
@@ -1,6 +1,6 @@
 import {
   game_results_array   
-} from '../data250715_2004.js';
+} from '../data250715_2025.js';
 
 const teamClassMap = {
   'SUD': 'score_sud',
--- registration/registration.js
+++ registration/registration.js
@@ -1,4 +1,4 @@
-import { players } from '../data250715_2004.js';
+import { players } from '../data250715_2025.js';
 console.log("inizio registration.js");
 
 // Group players by their team
